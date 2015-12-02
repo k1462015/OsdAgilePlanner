bd929f30046fffd150bde8fa4a2698463c3d5838
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-12-02 12:00:33 +0000

    Removed redundant code.

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,7 @@
  */
 public class Main {
     public static void main(String[] args){
-//        String operatingSystem = System.getProperty("os.name").toLowerCase();
-//        String fileSeperator = "/";
-//        if(operatingSystem.contains("mac")){
-//            fileSeperator = "\\";
-//        }
-//        System.out.println(System.getProperty("user.dir")+fileSeperator+"in.txt");
-//        String text = "t3.dependsOn";
-//        String[] split = text.split("\\.");
-//        System.out.println(split.length);
         GUI gui = new GUI();
-//        System.out.println("Hello world");
-//        Task t1 = new Task();
-//        t1.setDuration(1);
-//        Task t2 = new Task();
-//        t2.setDuration(2);
-//        Task t3 = new Task();
-//        t3.setDuration(3);
-//        Task t4 = new Task();
-//        t4.setDuration(4);
-//        Task t5 = new Task();
-//        t5.setDuration(5);
-//        Task t6 = new Task();
-//        t6.setDuration(6);
-//
-//        t1.addDependsOnTask(t2,t3,t4);
-//        t2.addDependsOnTask(t5,t6);
-//
-//        System.out.println(t1.totalDuration());
     }
 
 }
